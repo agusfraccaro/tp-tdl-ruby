96342e3190276bcbd11e8364ec660e9fa0360b3c
Agustina Fraccaro <afraccaro@fi.uba.ar>
2022-11-11 21:45:07 -0300

    corrijo signup view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,6 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
       redirect_to login_path, notice: 'Usuario creado satisfactoriamente.'
     else
       render :new
