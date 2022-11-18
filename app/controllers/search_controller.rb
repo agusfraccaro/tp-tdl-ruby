7276cc4ca849a9c4a3015cea8caa161aebecc577
Agustina Fraccaro <afraccaro@fi.uba.ar>
2022-11-18 20:19:53 -0300

    arreglos like

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  before_action :require_login
+  
   def index
     @query = Recipe.ransack(params[:q])
     @recipes = @query.result(distinct: true)
